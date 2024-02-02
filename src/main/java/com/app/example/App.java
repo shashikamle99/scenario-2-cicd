e2058c7eb2b7ccde040064db4795581d7011737f
shashikamle99 <154310776+shashikamle99@users.noreply.github.com>
2024-02-02 13:15:58 +0530

    Update App.java

--- src/main/java/com/app/example/App.java
+++ src/main/java/com/app/example/App.java
@@ -20,6 +20,6 @@ public class App extends SpringBootServletInitializer {
 
    @RequestMapping(value = "/")
    public String hello() {
-      return "<center> Welcome to Shashi Kamle..!!</center>";
+      return "<center> Welcome to DevOps Project..!!</center>";
    }
 }
